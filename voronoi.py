3491b189f5d1961e58db0057bba4970ade11b415
platonikc <superkittyfantastico@gmail.com>
2016-08-09 17:02:33 +0100

    Add files via upload

---
+++ voronoi.py
@@ -0,0 +1,15 @@
+import math
+
+def euclid(x,y):
+	return math.sqrt((x*x)+(y*y))
+
+class 2dpoint:
+    def __init__(self,x,y):
+        self.x = x
+        self.y = y
+    def getX():
+        return self.x
+    def getY():
+        return self.y
+    def euclidDist(x1,y1):
+        return euclid(x-x1,y-y1)
